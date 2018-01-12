f64c34aa5a85b130e4f6a491d514cf2fbd9efe4a
thofik <thofikwiranata15@gmail.com>
2018-01-12 14:11:19 +0700

    changed core , remove useless js

--- js/montase-tabs.js
+++ js/montase-tabs.js
@@ -8,95 +8,112 @@
 		$(el).on('click', tabs, this.show)
 	}
 
-	MontaseTabs.VERSION = '0.0.1[BETA]'
+	MontaseTabs.VERSION = '1.0.0[BETA]'
 
 	MontaseTabs.AUTHOR = 'thofikwiranata15@gmail.com'
 
 	MontaseTabs.prototype.show = function(e) {
+		var $this                 = $(this)
+    var $parent               = $this.closest(navTabs)
+    var $nav                  = $parent.find('.tabs-material__list')
+    var $parentContent        = $parent.next()
+    var indexListActiveBefore = getIndexListActive()
 
-		if(e) e.preventDefault()
-		MontaseTabs.prototype.active(this)
+    if(e) e.preventDefault()
+
+    $nav.removeClass('active')
+    $parentContent
+      .find('.tabs-material-item')
+      .removeClass('active')
+
+    var $currentList = $this.parent()
+    $currentList.addClass('active')
+
+    var space = getSpace()
+
+    animate($parent, $currentList, space)
+    setContentActive($this)
+
+    function getSpace() {
+      var currentIndex = $nav.index($currentList)
+      var space50 = getSpace50($parent)
+      var $list = $parent
+              .find('.tabs-material__list')
+              .eq(indexListActiveBefore)
+
+      var space = sumSpace()
+      return space50 + space
+
+      function sumSpace() {
+        var totalSpace = 0
+        for(var i = 0; i < currentIndex; i++) {
+          totalSpace  += $nav.eq(i).outerWidth()
+        }
+
+        return totalSpace
+      }
+
+    }
+
+    function getIndexListActive() {
+      var index = $parent
+              .find('.tabs-material__list.active')
+              .index()
+      return index
+    }
 	}
 
-	MontaseTabs.prototype.active = function(target) {
-		var $this = $(target)
-		var $parent = $this.closest(navTabs)
-		var $nav = $parent.find('.tabs-material__list')
-		var $parentContent = $parent.next()
-		var indexListActiveBefore = getIndexListActive()
+  MontaseTabs.prototype.setupTabsLine = function(parentNav) {
+    var $parent = $(parentNav)
+    // space50 => setengah space antara margin tabs dengan parent
+    var space50 = getSpace50($parent)
+    var $line = $parent.find('.tabs-material-line')
+    var widthList = getFirstListWidth()
+        
+    $line
+      .css({
+        transform: 'translate3d(' + space50 + 'px,0,0)',
+        width: widthList
+    })
 
-		$nav.removeClass('active')
-		$parentContent
-			.find('.tabs-material-item')
-			.removeClass('active')
+    function getFirstListWidth() { 
+      return $parent
+          .find('.tabs-material__list')
+          .first()
+          .width()
+    }
+  }
 
-		var $currentList = $this.parent()
-		$currentList.addClass('active')
+  function animate($parent, $currentList, space) {
+    var $line = $parent.find('.tabs-material-line')
+    var widthList = getWidth($currentList) 
 
-		var space = getSpace()
+    $line
+      .css({
+        transform: 'translate3d(' + space + 'px,0,0)',
+        width: widthList
+      })
 
-		MontaseTabs.prototype.animate($parent, $currentList, space)
-		MontaseTabs.prototype.setContentActive($this)
+    function getWidth($currentList) {
+      return $currentList.width()
+    }
+  }
 
-		function getSpace() {
-			var currentIndex = $nav.index($currentList)
-			var space50 = MontaseTabs.prototype.getSpace50($parent)
-			var $list = $parent
-							.find('.tabs-material__list')
-							.eq(indexListActiveBefore)
+  function setContentActive($target) {
+    var contentID = $target.attr('href')
+    $(contentID)
+        .addClass('active')
+        .addClass('is-tiny')
+        
+    setTimeout(function(){
+      $(contentID).removeClass('is-tiny')
+    }, 50);
+  }
 
-			var space = sumSpace()
-			return space50 + space
-
-			function sumSpace() {
-				var totalSpace = 0
-				for(var i = 0; i < currentIndex; i++) {
-					totalSpace  += $nav.eq(i).outerWidth()
-				}
-
-				return totalSpace
-			}
-
-		}
-
-		function getIndexListActive() {
-			var index = $parent
-							.find('.tabs-material__list.active')
-							.index()
-			return index
-		}
-	}
-
-	MontaseTabs.prototype.animate = function($parent, $currentList, space) {
-		var $line = $parent.find('.tabs-material-line')
-		var widthList = getWidth($currentList) 
-
-		$line
-			.css({
-				transform: 'translate3d(' + space + 'px,0,0)',
-				width: widthList
-			})
-
-		function getWidth($currentList) {
-			return $currentList.width()
-		}
-	}
-
-	MontaseTabs.prototype.setContentActive = function($target) {
-		var contentID = $target.attr('href')
-		$(contentID)
-				.addClass('active')
-				.addClass('is-tiny')
-				
-		setTimeout(function(){
-			$(contentID).removeClass('is-tiny')
-		}, 50);
-	}
-
-	MontaseTabs.prototype.getSpace50 = function($parent) {
+	function getSpace50($parent) {
 		var haveGutter = $parent.attr('data-tabs-gutter')
 
-		var gutter = haveGutter === undefined ? 0 : haveGutter
+		var gutter = typeof haveGutter === 'undefined' ? 0 : haveGutter
 		// space => setengah space antara margin tabs dengan parent
 		var space = (getParentWidth() - getTabsWidth()) / 2
 		return parseInt(space) + parseInt(gutter)
@@ -111,27 +128,6 @@
 		}
 	}
 
-	MontaseTabs.prototype.setupTabsLine = function(parentNav) {
-		var $parent = $(parentNav)
-		// space50 => setengah space antara margin tabs dengan parent
-		var space50 = this.getSpace50($parent)
-		var $line = $parent.find('.tabs-material-line')
-		var widthList = getFirstListWidth()
-				
-		$line
-			.css({
-				transform: 'translate3d(' + space50 + 'px,0,0)',
-				width: widthList
-		})
-
-		function getFirstListWidth() { 
-			return $parent
-					.find('.tabs-material__list')
-					.first()
-					.width()
-		}
-	}
-
 	// TABS MATERIAL PLUGIN DEFINITION
 	// ========================
 	function Plugin() {
@@ -140,7 +136,7 @@
 			var data    = $this.data('mt.montasetabs')
 				    
 			if (!data) $this.data('mt.montasetabs', (data = new MontaseTabs(this)))
-			if (typeof option == 'number') data.to(option)
+			if (typeof option == 'string') data[option]
 		})
 	}
 
